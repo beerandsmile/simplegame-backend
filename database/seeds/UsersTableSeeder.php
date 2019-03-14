1608168dbbeafe41730715c2734b7420c342a135
Russi4nBe4r <mterek97@gmail.com>
2019-03-14 04:26:40 +0300

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@buhojmedved.ru',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
