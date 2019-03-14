c97fc7df2f2471dc74d2104bc1e8ba231bae0e7d
Russi4nBe4r <mterek97@gmail.com>
2019-03-14 04:34:05 +0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('/v1')->group(function() {
+    Route::post('login', 'Api\AuthController@login');
+
+    Route::middleware('token')->group(function() {
+        Route::get('/games', 'Global\Api\GameController@list');
+
+        Route::post('/games', 'Global\Api\GameController@create');
+
+        Route::get('/games/{hash}', 'Global\Api\GameController@game');
+
+        Route::post('/games/{hash}', 'Global\Api\GameController@move');
+
+        Route::get('/users', 'Global\Api\UserController@list');
+    });
+});
